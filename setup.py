b7c48cf5fe49ceb6150da3da218097282aef6b39
Roman Rizvanov <progbeat@users.noreply.github.com>
2020-06-03 18:05:11 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,3 @@ import getpass
 
 x = getpass.getpass('Enter password: ')
 print('Test', x)
-
-from setuptools import setup, find_packages
-
-setup(
-    name='colab_env',
-    version='1.0.0',
-    url='https://github.com/itesoro/colab_env',
-    packages=find_packages()
-)
