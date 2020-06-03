0e8d60e2c1b69bfde567521022d93b11fcf9f98a
Roman Rizvanov <progbeat@users.noreply.github.com>
2020-06-03 18:03:20 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
 print('Test')
+
+from setuptools import setup, find_packages
+
+setup(
+    name='colab_env',
+    version='1.0.0',
+    url='https://github.com/itesoro/colab_env',
+    packages=find_packages()
+)
