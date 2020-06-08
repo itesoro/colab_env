c8f35de7aa7824f91681d9ea96b8f459bd770d3c
Roman Rizvanov <progbeat@users.noreply.github.com>
2020-06-08 11:50:06 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,34 +26,36 @@ def main():
         }
         config['user'] = user
         update_config = True
+    user_name = user['name']
+    user_email = user['email']
+    os.system(f'git config --global user.name "{user_name}"')
+    os.system(f'git config --global user.email "{user_email}"')
     
     passphrase = None
 
     def enter_passphrase():
         nonlocal passphrase
-        passphrase = getpass.getpass(f'Enter passphrase for key "{KEY_PATH}": ')
+        while True:
+            passphrase = getpass.getpass(f'Enter passphrase for key "{KEY_PATH}": ')
+            if len(passphrase) >= 5:
+                return
+            print('Passphrase is too short (minimum five characters)')
 
     enter_passphrase()
-    if not os.path.isfile(KEY_PATH):
-        while True:
-            if passphrase == getpass.getpass(f'Confirm passphrase: '):
-                break
-            enter_passphrase()
-        email = user['email']
-        while True:
-            ret_code = os.system(f'ssh-keygen -t rsa -b 4096 -C "{email}" -f "{KEY_PATH}" -N {passphrase}')
+    while not os.path.isfile(KEY_PATH)::
+        if passphrase == getpass.getpass(f'Confirm passphrase: '):
+            ret_code = os.system(f'ssh-keygen -t rsa -b 4096 -C "{user_email}" -f "{KEY_PATH}" -N {passphrase}')
             if ret_code == 0:
                 print(f'Saved keys to "{SSH_DIR}"')
                 break
-            assert ret_code == 256, f'{ret_code}'
-            print(f'Saving key "{KEY_PATH}" failed: passphrase is too short (minimum five characters)')
-            enter_passphrase()
+        enter_passphrase()
     
     os.system('mkdir -p /root/.ssh')
     os.system(f'cp -f "{SSH_DIR}/id_rsa" /root/.ssh/id_rsa')
     os.system('ssh-keyscan github.com >> /root/.ssh/known_hosts')
     os.system('chmod 600 /root/.ssh/id_rsa')
     os.system('chmod 644 /root/.ssh/known_hosts')
+
     while True:
         ret_code = os.system(f'ssh-keygen -p -P "{passphrase}" -N "" -f /root/.ssh/id_rsa')
         if ret_code == 0:
