790a46d6c2392a3512c59370fa3668bb0bd96a3c
Roman Rizvanov <progbeat@users.noreply.github.com>
2020-06-17 17:41:49 +0300

    Hide drive mount point and make symlinks.

--- setup_xp.py
+++ setup_xp.py
@@ -7,9 +7,37 @@ def main():
     import getpass
     import shutil
     from IPython.display import clear_output
+
+    DRIVE_DIR = '/content/drive'
+    HIDDEN_DRIVE_DIR = '/content/.drive'
+
+    def hide_drive_mount_point():
+        nonlocal DRIVE_DIR, HIDDEN_DRIVE_DIR
+        if DRIVE_DIR == HIDDEN_DRIVE_DIR:
+            return
+        ok = True
+        if not os.path.isdir(HIDDEN_DRIVE_DIR):
+            ok = ok and os.system(f'mkdir -p "{HIDDEN_DRIVE_DIR}"') == 0
+            ok = ok and os.system(f'mount --move "{DRIVE_DIR}" "{HIDDEN_DRIVE_DIR}"') == 0
+            if ok:
+                ok = os.system(f'rmdir "{DRIVE_DIR}"') == 0
+            else:
+                ok = os.system(f'rmdir "{HIDDEN_DRIVE_DIR}"') == 0
+        if ok:
+            DRIVE_DIR = HIDDEN_DRIVE_DIR
+    
+    def link(path):
+        new_path = f'/content/{os.path.basename(path)}'
+        if os.path.islink(new_path):
+            return new_path
+        if os.system(f'ln -s "{path}" "{new_path}"') == 0:
+            return new_path
+        return path
+
+    hide_drive_mount_point()
     sys.argv = sys.argv[1:]
-    MY_DRIVE_DIR = '/content/drive/My Drive'
-    config = {'MY_DRIVE_DIR': MY_DRIVE_DIR}
+    MY_DRIVE_DIR = f'{DRIVE_DIR}/My Drive'
+    config = {'MY_DRIVE_DIR': MY_DRIVE_DIR, 'link': link}
     config_path = os.path.join(MY_DRIVE_DIR, sys.argv[0])
     exec(open(config_path).read(), config)
     USER_NAME = config['USER_NAME']
@@ -20,7 +48,7 @@ def main():
     DST_SSH_DIR = os.path.dirname(DST_KEY_PATH)
     os.system(f'git config --global user.name "{USER_NAME}"')
     os.system(f'git config --global user.email "{USER_EMAIL}"')
-    
+
     passphrase = None
 
     def enter_passphrase():
